da5724a7ed803bb9c0cb9ee089083ef7353a0874
Jens <jk224vx@student.lnu.se>
2025-09-02 19:12:26 +0200

    First commit

---
+++ greeting.js
@@ -0,0 +1,15 @@
+/** 
+ * Greets the user with a friendly message.
+ */
+
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import chalk from 'chalk';
+
+const rl = readline.createInterface({ input, output });
+
+const name = await rl.question("Vad heter du?: ");
+
+console.log(chalk.blue("Hej " + name + "!"));
+
+rl.close();
