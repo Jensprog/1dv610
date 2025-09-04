5e812b041c76d56a13b6f31eeff45791e6b1ea44
Jens <jk224vx@student.lnu.se>
2025-09-04 14:13:37 +0200

    Changes to greeting.js

--- src/App.java
+++ src/App.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner greeting = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Vad heter du?: ");
 
-        String name = greeting.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Hej " + name + "!🫡");
-        greeting.close();
+        scanner.close();
     }
     
 }
--- src/greeting.js
+++ src/greeting.js
@@ -2,18 +2,10 @@
  * Greets the user with a friendly message.
  */
 
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-import chalk from 'chalk';
-
-const rl = readline.createInterface({ input, output });
-
-const name = await rl.question(chalk.yellow("Vad heter du?: "));
-
-console.log(chalk.blue("Hej " + name + "!🫡"));
-
-await rl.question(chalk.green("Vilken kurs ska du gå?: "));
-
-console.log(chalk.red("Kul! Lycka till😎"));
-
-rl.close();
+function displayGreeting() {
+    const name = document.getElementById("name").value;
+    const greeting = `Hej ${name}! Trevligt att träffas!🫡`;
+    const fact = "Visste du att flodhästar inte kan simma?🦛";
+    document.getElementById("greeting").textContent = `${greeting}`;
+    document.getElementById("fact").textContent = `${fact}`;
+}
